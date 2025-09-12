b04c9073e854c1a868875bb267d7d7c7274b95d5
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-12 11:54:29 +0530

    Add files via upload

---
+++ conditional.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        int age = 20;
+        string result = (age >= 18) ? "Adult" : "Minor";
+        Console.WriteLine(result);
+    }
+}
