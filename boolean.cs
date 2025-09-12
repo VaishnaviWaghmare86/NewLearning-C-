e645db46e05d909babae10bef775474931f164be
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-12 11:54:50 +0530

    Add files via upload

---
+++ boolean.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+    static void Main() {
+        bool isStudent = true;
+        bool hasPassed = false;
+
+        Console.WriteLine("Student? " + isStudent);
+        Console.WriteLine("Passed? " + hasPassed);
+    }
+}
