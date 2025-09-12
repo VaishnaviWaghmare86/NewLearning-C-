ebb541cee51dc2ab8f5c55fefa4cc3f1cb25ccda
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-12 12:03:36 +0530

    Add files via upload

---
+++ consoleIO.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        Console.Write("Enter your city: ");
+        string city = Console.ReadLine();
+
+        Console.WriteLine("You live in " + city);
+    }
+}
