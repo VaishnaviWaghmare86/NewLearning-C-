a21d3f96074ec141db1c41983fc0331c2771b66c
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-12 11:55:48 +0530

    Add files via upload

---
+++ var.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        var name = "Vaishu"; // string
+        var age = 21;        // int
+
+        Console.WriteLine($"{name} is {age} years old.");
+    }
+}
