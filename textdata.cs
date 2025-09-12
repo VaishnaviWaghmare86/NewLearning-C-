41baa7bf5f3a5cb959f056e57c79ed06de4fb1ae
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-12 12:02:32 +0530

    Add files via upload

---
+++ textdata.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+    static void Main() {
+        string name = "Vaishu";
+        char grade = 'A';
+
+        Console.WriteLine("Name: " + name);
+        Console.WriteLine("Grade: " + grade);
+    }
+}
