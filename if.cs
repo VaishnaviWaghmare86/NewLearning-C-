017523a78bbfce156652b0995ef4f20ffc8e1927
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-12 11:52:54 +0530

    Add files via upload

---
+++ if.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program {
+    static void Main() {
+        int age = 18;
+
+        if (age >= 18)
+            Console.WriteLine("Adult");
+        else
+            Console.WriteLine("Minor");
+    }
+}
