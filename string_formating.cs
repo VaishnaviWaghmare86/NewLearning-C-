ef8f12a7ff7d14f35a93fa967e7fb429d8e9fbba
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-22 19:25:11 +0530

    Add files via upload

---
+++ string_formating.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        string name = "Vaishu";
+        int age = 21;
+        Console.WriteLine(string.Format("My name is {0}, age {1}", name, age));
+    }
+}
