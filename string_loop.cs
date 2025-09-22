75a809b893824f5ac6f9313f60dc9cb2bf3b159c
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-22 19:24:20 +0530

    Add files via upload

---
+++ string_loop.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        string word = "Vaishu";
+        foreach (char c in word) {
+            Console.WriteLine(c);
+        }
+    }
+}
