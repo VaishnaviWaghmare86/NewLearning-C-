ec143e435837be40182d1cbe98086eb6a79bff1c
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-22 19:25:54 +0530

    Add files via upload

---
+++ string_concat.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Program {
+    static void Main() {
+        string first = "Hello";
+        string second = "World";
+        string result = first + " " + second;
+        Console.WriteLine(result);
+    }
+}
