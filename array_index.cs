bbf304746e98048dfa5357fb6c7a4f606fa7d397
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-23 09:27:01 +0530

    Add files via upload

---
+++ array_index.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        int[] nums = {10, 20, 30};
+        int index = Array.IndexOf(nums, 20);
+        Console.WriteLine("Index of 20: " + index);
+    }
+}
