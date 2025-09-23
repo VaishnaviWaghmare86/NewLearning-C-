949b5da49a2fe55ac752a9030e6d8793ad50b777
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-23 09:49:55 +0530

    Add files via upload

---
+++ string_interpolation.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+    static void Main() {
+        string name = "Vaishu";
+        int age = 21;
+        Console.WriteLine($"My name is {name}, age {age}");
+    }
+}
