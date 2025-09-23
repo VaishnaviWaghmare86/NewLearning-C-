2d985145e02be83ce8eeddcf1cc53627146f9dda
Vaishnavi Waghmare <waghmarevaishnavi40@gmail.com>
2025-09-23 09:24:39 +0530

    Add files via upload

---
+++ returnfucn.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Program {
+    static int Square(int x) {
+        return x * x;
+    }
+
+    static void Main() {
+        Console.WriteLine("Square of 5: " + Square(5));
+    }
+}
